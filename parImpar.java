b9fcd5637c99b25a0b69f845a150648ca9b272b4
Bryan Franco <bryanfranco_94@hotmail.com>
2020-01-10 16:56:47 -0600

    Programa en java que dice si el numero ingresado es par o impar

---
+++ parImpar.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class parImpar {
+	
+	static boolean isPar(int numero) {
+		boolean isPar = (numero%2==0)? true : false;
+		return isPar;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num=0;
+		int result=0;
+		String pregunta=null;		
+		do {
+			System.out.print("Ingresa un numero: ");
+			num=sc.nextInt();
+			result=(isPar(num)==true)? 1: 0;
+			switch(result) {
+			case 0: 
+				System.out.println("Es impar");
+				break;
+			case 1:
+				System.out.println("Es par");
+				break;
+				default:
+					System.out.println("No se supo definir");
+					break;
+			}
+			sc.nextLine();
+			System.out.print("Desea repetir? (s,n): ");
+			pregunta=sc.nextLine();
+		}while(pregunta.charAt(0)=='s' || pregunta.charAt(0)=='S');
+		
+		
+		
+	}
+
+}
